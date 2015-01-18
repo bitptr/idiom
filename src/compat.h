fce74d6c433b63fd23d056217a8fafc132882bb6
Mike Burns <mike@mike-burns.com>
2015-01-18 22:53:52 +0100

    Add strlcpy(3) and reallocarray(3)
    GNU libc does not ship with strlcpy(3), and many libcs do not yet ship
    with reallocarray(3), so provide them with this project.

--- src/compat.h
+++ src/compat.h
@@ -17,4 +17,14 @@
 #endif
 #endif /* __linux__ || __CYGWIN__ */
 
+#ifndef HAVE_STRLCPY
+#include <sys/types.h>
+size_t	strlcpy(char *, const char *, size_t);
+#endif /* !HAVE_STRLCPY */
+
+#ifndef HAVE_REALLOCARRAY
+#include <sys/types.h>
+void	*reallocarray(void *optr, size_t nmemb, size_t size)
+#endif /* !HAVE_REALLOCARRAY */
+
 #endif /* COMPAT_H */
